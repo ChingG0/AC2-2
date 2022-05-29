80e722990a7d0302e126af190120b2a137dfeb3a
ChingG0 <www.karta2428620@gmail.com>
2022-05-29 15:34:02 +0800

    fakeUserList

--- A8/index2.js
+++ A8/index2.js
@@ -1,13 +1,16 @@
-const BASE_URL = 'https://lighthouse-user-api.herokuapp.com'
-const INDEX_URL = BASE_URL + '/api/v1/users'
+const BASE_URL = "https://lighthouse-user-api.herokuapp.com";
+const INDEX_URL = BASE_URL + "/api/v1/users";
+const AVATARS_PER_PAGE = 18;
 
-const dataPanel = document.querySelector('#data-panel')
+const dataPanel = document.querySelector("#data-panel");
+const paginator = document.querySelector("#paginator");
 
-Avatars = []
+Avatars = [];
 
-function renderAvatarList(data){
-    let rawHTML = ''
-    data.forEach((item)=>{
+//render畫面
+function renderAvatarList(data) {
+  let rawHTML = "";
+  data.forEach((item) => {
     rawHTML += `<div class="col-sm-2 mt-3">
                     <div class="card">
                         <img src="${item.avatar}"    class="card-img-top"  alt="avatar">
@@ -15,12 +18,36 @@ function renderAvatarList(data){
                             <h6 class="card-title" dir="ltr">${item.name}${item.id}</h6>
                         </div>
                     </div>
-                </div>`
-    })
-    dataPanel.innerHTML = rawHTML
+                </div>`;
+  });
+  dataPanel.innerHTML = rawHTML;
 }
 
-axios.get(INDEX_URL).then((res)=>{
-    Avatars.push(...res.data.results)
-    renderAvatarList(Avatars)
-})
+function getAvatarsByPages(page) {
+  const startIndex = (page - 1) * AVATARS_PER_PAGE
+  return Avatars.slice(startIndex, startIndex + AVATARS_PER_PAGE)
+}
+
+//將資料分頁
+function renderPaginator(amount) {
+  const numberOfPage = Math.ceil(amount / AVATARS_PER_PAGE);
+  let rawHTML = "";
+  for(let page = 1; page <= AVATARS_PER_PAGE; page++){
+    rawHTML += `<li class="page-item"><a class="page-link" href="#" data-page="${page}">${page}</a></li>`;  
+  }
+  paginator.innerHTML = rawHTML
+}
+
+function onPaginatorClicked(event) {
+  if (event.target.tagName !== "A") return;
+  const page = Number(event.target.dataset.page);
+  renderAvatarList(getAvatarsByPages(page));
+}
+
+axios.get(INDEX_URL).then((res) => {
+  Avatars.push(...res.data.results);
+  renderPaginator(Avatars.length)
+  renderAvatarList(getAvatarsByPages(1));
+});
+
+paginator.addEventListener("click", onPaginatorClicked);
