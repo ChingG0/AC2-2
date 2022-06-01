36d84914a3417c3f47de3e420273d14fa39753c4
ChingG0 <www.karta2428620@gmail.com>
2022-06-01 23:15:16 +0800

    Memorizing Game

--- U35/card.js
+++ U35/card.js
@@ -1,41 +1,77 @@
 const Symbols = [
-    'https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png', // ¶Â®ç
-    'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678087-heart-512.png', // ·R¤ß
-    'https://cdn-icons-png.flaticon.com/512/458/458518.png', // ¤è¶ô
-    'https://assets-lighthouse.alphacamp.co/uploads/image/file/17988/__.png' // ±öªá
-  ]
+  "https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png", // ï¿½Â®ï¿½
+  "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678087-heart-512.png", // ï¿½Rï¿½ï¿½
+  "https://cdn-icons-png.flaticon.com/512/458/458518.png", // ï¿½ï¿½ï¿½
+  "https://assets-lighthouse.alphacamp.co/uploads/image/file/17988/__.png", // ï¿½ï¿½ï¿½ï¿½
+];
 const view = {
-   
-    //transform ¯S®íµP
-    transformNumber(number){
-        switch (number) {
-          case 1:
-            return 'A'
-          case 11:
-            return 'J'
-          case 12:
-            return 'Q'
-          case 13:
-            return 'K'
-          default:
-            return number
-        }
-    },
-    //render µe­±
-    getCardElement(index){
-        const number = this.transformNumber((index % 13) + 1)
-        const symbol = Symbols[Math.floor(index / 13)]
-        return `<div class="card back">
-        <p>${number}</p>
-        <img src="${symbol}">
-        <p>${number}</p>
-    </div>`
-    },
-    //¾É¤J52±i
-    displayCards(){
-        const rootElement = document.querySelector('#cards')
-        rootElement.innerHTML = Array.from(Array(52).keys()).map(index => this.getCardElement(index)).join('')
+  //transform specialNumber
+  transformNumber(number) {
+    switch (number) {
+      case 1:
+        return "A";
+      case 11:
+        return "J";
+      case 12:
+        return "Q";
+      case 13:
+        return "K";
+      default:
+        return number;
     }
-}
+  },
+  //
+  getCardContent(index) {
+    const number = this.transformNumber((index % 13) + 1);
+    const symbol = Symbols[Math.floor(index / 13)];
 
-view.displayCards()
+    return `  <p>${number}</p>
+      <img src="${symbol}">
+      <p>${number}</p>
+  </div>`;
+  },
+  //render view
+  getCardElement(index) {
+    return `<div data-index = '${index}' class="card back"></div>`;
+  },
+
+  displayCards() {
+    const rootElement = document.querySelector("#cards");
+    rootElement.innerHTML = utility
+      .getRandomNumberArray(52)
+      .map((index) => this.getCardElement(index))
+      .join("");
+  },
+
+  flipCard(card) {
+    if (card.classList.contains("back")) {
+      card.classList.remove("back");
+      card.innerHTML = this.getCardContent(Number(card.dataset.index));
+      return;
+    }
+    card.classList.add("back");
+    card.innerHTML = null;
+  },
+};
+
+const utility = {
+  getRandomNumberArray(count) {
+    const number = Array.from(Array(count).keys());
+    for (let index = number.length - 1; index > 0; index--) {
+      let randomIndex = Math.floor(Math.random() * (index + 1));
+      [number[index], number[randomIndex]] = [
+        number[randomIndex],
+        number[index],
+      ];
+    }
+    return number;
+  },
+};
+
+view.displayCards();
+
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("click", (event) => {
+    view.flipCard(card);
+  });
+});
